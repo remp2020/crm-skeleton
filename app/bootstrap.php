043e773cd087f29a433fa7c0fae517123ea73369
Miroslav Svítok <mirosvitok@gmail.com>
2020-06-30 10:31:18 +0200

    Resolution of app dir added
    remp/crm#452

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-$application = new Crm\ApplicationModule\Core();
+$application = new Crm\ApplicationModule\Core(dirname(__DIR__));
 return $application->bootstrap();
